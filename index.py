90e1ad9979402eb6766dd895528a2e09e3cace3f
Ann Starovoitov <annstarovoitov@gmail.com>
2022-08-16 12:06:53 -0400

    Add files via upload

--- index.py
+++ index.py
@@ -1,1 +1,28 @@
-print('Lists')
+
+friends = ['Hanna', 'Oliver', 'Jama', 'Dan']
+print(friends) # we print the LIST of friends 
+
+# to access the list you can add index
+
+print(friends[1], friends[2])
+# index starts with 0
+
+print(friends[3], friends[1])
+
+print(friends[-2])
+
+# first from the end
+print(friends[-1])
+
+# slicing method with :
+print(friends[1:3]) # will print #1 and 2 excluding 3
+
+# slicing method with :
+print(friends[1:4]) # will print #1 to 3 excluding 4
+
+# slicing method with :
+print(friends[:]) # will print the whole list
+
+print(len(friends))
+print(friends.index('Oliver'))
+

8b56fb76352f2a4d967c4139a99d16b33849504e
Ann Starovoitov <annstarovoitov@gmail.com>
2022-08-23 10:40:50 -0400

    Add files via upload

--- index.py
+++ index.py
@@ -136,3 +136,12 @@ print(friends)
 print(new_friends)
 
 
+#split and join 
+
+msg ='Welcome  to  Python  101: Split  and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(''.join(friends_list))
+
+
+print(''.join(msg.split()))
